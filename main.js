e898fb71e50f1dc1e8fafff1ac0924d1be8a1f1e
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-06-14 20:00:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var horror = "Alien"; //I'm assigning the word horror that equals Alien//
+var horror = horror.fontcolor("green"); //I'm assigning the color green for horror//
+const str = 'Directed By' + 'Ridley' + 'Scott';
+str; //string includes concatenate//
+document.write(horror); //because the word horror has been assigned green and horror equals alien, the alien word will be in green font//
+document.write("\"In space," + " no one can hear you scream\"");//here is the concatenate//
+document.write(2021 - 42) //this was the year alien was released, this is an expression //
